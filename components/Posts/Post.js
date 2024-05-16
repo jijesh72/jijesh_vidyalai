c842208163afe6965c76949edbea91bec7ae6e6b
Jijesh <jijeshjiju0272@gmail.com>
2024-05-16 10:29:46 +0530

    complete the 4  tasks

--- components/Posts/Post.js
+++ components/Posts/Post.js
@@ -69,7 +69,7 @@ const Post = ({ post }) => {
   const handleNextClick = () => {
     if (carouselRef.current) {
       carouselRef.current.scrollBy({
-        left: 50,
+        left: 300,
         behavior: 'smooth',
       });
     }
@@ -78,7 +78,7 @@ const Post = ({ post }) => {
   const handlePrevClick = () => {
     if (carouselRef.current) {
       carouselRef.current.scrollBy({
-        left: -70,
+        left: -420,
         behavior: 'smooth',
       });
     }
